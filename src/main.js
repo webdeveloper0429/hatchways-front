58610ed1c5cdaf0b150f1691718ba850b938d73f
webdeveloper0429 <webdeveloper0429@yahoo.com>
2021-05-12 01:45:08 +0800

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue'
 
 import './assets/scss/index.scss';
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
     render: h => h(App),
