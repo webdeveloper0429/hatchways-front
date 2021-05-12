f821ccc87e94267747d0c0dea19235a3736b6f77
webdeveloper0429 <webdeveloper0429@yahoo.com>
2021-05-12 15:29:50 +0800

    part 3

--- src/main.js
+++ src/main.js
@@ -2,11 +2,27 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import vuescroll from 'vuescroll';
 
 import './assets/scss/index.scss';
 
 Vue.config.productionTip = false
 
+Vue.use(vuescroll, {
+    ops: {
+        vuescroll: {
+            wheelScrollDuration: 300,
+        },
+        scrollPanel: {
+            speed: 3000,
+            easing: 'easeInQuad'
+        },
+        rail: {},
+        bar: {
+            background: '#ccc',
+        }
+    },
+});
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
--- src/pages/Students.vue
+++ src/pages/Students.vue
@@ -1,53 +1,73 @@
 <template>
     <b-container class="p-5 h-100">
-        <div class=" h-100 shadow bg-white rounded">
+        <div class="shadow bg-white rounded h-100">
             <div class="search-container">
-                <div>
-
+                <div class="border-bottom">
+                    <b-form-input
+                        class="border-0 shadow-none"
+                        v-model="name"
+                        type="search"
+                        placeholder="Search by name"
+                    ></b-form-input>
+                </div>
+                <div class="border-bottom">
+                    <b-form-input
+                        class="border-0 shadow-none"
+                        v-model="tag"
+                        type="search"
+                        placeholder="Search by tag"
+                    ></b-form-input>
                 </div>
             </div>
-            <b-list-group class="h-100 rounded">
-                <vuescroll :ops="ops">
-                    <b-list-group-item v-for="student in students" :key="student.id" >
-                        <Student :data="student" />
-                    </b-list-group-item>
-                </vuescroll>
-            </b-list-group>
+            <div class="student-wrapper">
+                <vue-scroll>
+                    <b-list-group flush class="">
+                        <b-list-group-item v-for="student in filteredStudents(name, tag)" :key="student.id" >
+                            <Student :data="student" />
+                        </b-list-group-item>
+                    </b-list-group>
+                </vue-scroll>
+            </div>
         </div>
     </b-container>
 </template>
 
 <script>
-import vuescroll from 'vuescroll';
 import Student from '../components/Student'
 import StudentService from '../services/StudentService'
 export default {
     name: 'Students',
     components: {
-        vuescroll,
         Student
     },
     data() {
         return {
-            ops: {
-                vuescroll: {
-                    wheelScrollDuration: 300,
-                },
-                scrollPanel: {
-                    speed: 3000,
-                    easing: 'easeInQuad'
-                },
-                rail: {
-                },
-                bar: {
-                    background: '#ccc',
-                }
-            },
+            name: '',
+            tag: '',
             students: []
         }
     },
     async created() {
         this.students = await StudentService.getStudents();
+    },
+    computed: {
+        filteredStudents: function() {
+            return function(name, tag) {
+                return this.students.filter(student => {
+                    const fullName = student.firstName + ' ' + student.lastName;
+                    const tagString = (student.tags && student.tags.length > 0) ? student.tags.join('---') : '';
+                    if (!tag && !name) {
+                        return this.students
+                    } else if (!tag) {
+                        return fullName.toLowerCase().includes(name.toLowerCase())
+                    } else if (!name) {
+                        return tagString.toLowerCase().includes(tag.toLowerCase())
+                    } else {
+                        return fullName.toLowerCase().includes(name.toLowerCase()) && tagString.toLowerCase().includes(tag.toLowerCase())
+                    }
+                })
+            }
+        }
     }
 }
 </script>
